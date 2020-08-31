b5a4c4b52573594b0d738790c4866446e039ca2b
John VanSickle <jwvansickle@gmail.com>
2020-08-31 14:51:44 -0400

    Initial Commit

---
+++ AXInject2041Chars/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AXInject2041Chars
+//
+//  Created by John VanSickle on 8/31/20.
+//  Copyright © 2020 John VanSickle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
